8911ac99793a49ccbd45b43854a85e4a61833cc8
haesooKr <gotn676@gmail.com>
2020-06-17 18:15:16 -0400

    Create NewTodoForm & css

---
+++ src/todos/NewTodoForm.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NewTodoForm = () => (
+  <div>
+    <div className="new-todo-form">
+      <input className="new-todo-input" type="text" />
+      <button className="new-todo-button">Create Todo</button>
+    </div>
+  </div>
+);
+
+export default NewTodoForm;
