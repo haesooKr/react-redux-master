6d140b93e81785e40790d57b88026ffb6e80265b
haesooKr <gotn676@gmail.com>
2020-06-17 20:26:37 -0400

    Link redux store to index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
-import React from 'react';
-import ReactDom from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDom from "react-dom";
+import { Provider } from "react-redux";
+import { configureStore } from "./store";
+import App from "./App";
 
-ReactDom.render(<App/>, document.getElementById('root'));
+ReactDom.render(
+  <Provider store={configureStore()}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
