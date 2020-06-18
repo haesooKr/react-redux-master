100043a5de73df55c96a3f4f6d2ae488f8150f1c
haesooKr <gotn676@gmail.com>
2020-06-18 13:40:10 -0400

    Add LOAD_TODOS Actions to thunk.js

--- src/todos/thunk.js
+++ src/todos/thunk.js
@@ -1,3 +1,22 @@
-export const displayAlert = text => () => {
+import {
+  loadTodoSuccess,
+  loadTodosInProgess,
+  loadTodosFailure,
+} from "./actions";
+
+export const loadTodos = () => async (dispatch, getState) => {
+  // we can recycle dispatch & get current State through thunk
+  try {
+    dispatch(loadTodosInProgess());
+    const response = await fetch("http://localhost:8080/todos-delay");
+    const todos = await response.json();
+    dispatch(loadTodoSuccess(todos));
+  } catch (e) {
+    dispatch(loadTodosFailure());
+    dispatch(displayAlert(e));
+  }
+};
+
+export const displayAlert = (text) => () => {
   alert(`You clicked on ${text}`);
-}
+};
