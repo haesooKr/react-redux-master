77685f1a1ccad857305df72fa934b1b11ac90d3e
haesooKr <gotn676@gmail.com>
2020-06-18 13:40:00 -0400

    Add LOAD_TODOS Actions

--- src/todos/actions.js
+++ src/todos/actions.js
@@ -8,10 +8,27 @@ export const REMOVE_TODO = "REMOVE_TODO";
 export const removeTodo = text => ({
   type: REMOVE_TODO,
   payload: { text },
-})
+});
 
 export const COMPLETE_TODO = "COMPLETE_TODO";
 export const completeTodo = text => ({
   type: COMPLETE_TODO,
   payload: { text },
+});
+
+export const LOAD_TODOS_IN_PROGRESS = "LOAD_TODOS_IN_PROGRESS";
+export const loadTodosInProgess = () => ({
+  type: LOAD_TODOS_IN_PROGRESS,
+  // it is not needed payload
+})
+
+export const LOAD_TODOS_SUCCESS = "LOAD_TODOS_SUCCESS";
+export const loadTodoSuccess = todos => ({
+  type: LOAD_TODOS_SUCCESS,
+  payload: { todos },
+})
+
+export const LOAD_TODOS_FAILURE = "LOAD_TODOS_FAILURE";
+export const loadTodosFailure = () => ({
+  type: LOAD_TODOS_FAILURE,
 })
