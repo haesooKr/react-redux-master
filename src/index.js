107b9778b2cf07c05d96eab58e709270fa140db1
haesooKr <gotn676@gmail.com>
2020-06-17 21:23:34 -0400

    Adapt to redux-persist

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from "react";
 import ReactDom from "react-dom";
+import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/lib/integration/react';
 import { Provider } from "react-redux";
 import { configureStore } from "./store";
 import App from "./App";
 
+const store = configureStore();
+const persistor = persistStore(store);
+
 ReactDom.render(
-  <Provider store={configureStore()}>
-    <App />
+  <Provider store={store}>
+    <PersistGate persistor={persistor} loading={<div>Loading...</div>}>
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById("root")
 );
