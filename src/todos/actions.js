79763985632e7f72234804df07d98409683d0073
haesooKr <gotn676@gmail.com>
2020-06-18 14:50:01 -0400

    Modify COMPLETE_TODO

--- src/todos/actions.js
+++ src/todos/actions.js
@@ -11,9 +11,9 @@ export const removeTodo = todo => ({
 });
 
 export const COMPLETE_TODO = "COMPLETE_TODO";
-export const completeTodo = text => ({
+export const completeTodo = todo => ({
   type: COMPLETE_TODO,
-  payload: { text },
+  payload: { todo },
 });
 
 export const LOAD_TODOS_IN_PROGRESS = "LOAD_TODOS_IN_PROGRESS";
