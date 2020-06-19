dfe2446a184ff8e6de2c85430ba9a7c99fd32416
haesooKr <gotn676@gmail.com>
2020-06-19 14:22:09 -0400

    Add test files

---
+++ src/todos/tests/thunks.test.js
@@ -0,0 +1,32 @@
+import "node-fetch";
+import fetchMock from "fetch-mock";
+import { expect } from "chai";
+import sinon from "sinon";
+import { loadTodos } from "../thunk";
+
+describe("The loadTodos thunk", () => {
+  it("Dispatches the correct actions in the success scenario", async () => {
+    const fakeDispatch = sinon.spy();
+
+    const fakeTodos = [{ text: "1" }, { text: "2" }];
+    fetchMock.get("http://localhost:8080/todos", fakeTodos);
+    // When thunk tries to use fetch to send a get request to this URL,
+    // it will just get back the fakeTodos instead of sending a real request.
+
+    const expectedFirstAction = { type: "LOAD_TODOS_IN_PROGRESS" };
+    const expectedSecondAction = {
+      type: "LOAD_TODOS_SUCCESS",
+      payload: {
+        todos: fakeTodos,
+      },
+    };
+
+    await loadTodos()(fakeDispatch);
+
+    expect(fakeDispatch.getCall(0).args[0]).to.deep.equal(expectedFirstAction);
+    expect(fakeDispatch.getCall(1).args[0]).to.deep.equal(expectedSecondAction);
+
+    fetchMock.reset();
+    // restore fetch to original state
+  });
+});
